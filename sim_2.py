74153fce334201324e4eea3d2c9d64410ee86ebf
Marek Kovac <marekkovac321@gmail.com>
2022-01-24 16:21:30 +0100

    working fine

--- sim_2.py
+++ sim_2.py
@@ -3,25 +3,10 @@ import numpy as np
 import time
 
 torch.manual_seed(0)
-class Test():
-        def __init__(self):
-                pass
 
-        def compute(self):
-                x = torch.tensor([[ 13.6019, -11.2565,   0.0566],
-                        [  7.7608,  -0.9985,  -9.0562],
-                        [ -2.5147,  18.5134,  -8.0817],
-                        [  1.1400,   0.2767,   2.8587],
-                        [ -7.9098,   1.7254,   6.1462],
-                        [-12.0256,  -1.2363,  -4.8732],
-                        [  0.5822,   8.1032,  13.4576]], device='cuda:0')
-
-                y = torch.ones(3,7)
-                return x,y
-
-ins = Test()
-print(ins.compute()[1])
+t = torch.Tensor([[1, 2, 3], [4,5,2]])
+print((t < 2.0).nonzero(as_tuple=True))
 
 
-
-
+a = torch.rand(1000000,1000)
+print(a)
