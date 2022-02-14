decd305388cbff9d932c4eb3e1555d78ec02b69d
Marek Kovac <marekkovac321@gmail.com>
2022-02-15 00:06:10 +0100

    3.6, not great, not terrible

--- sim_2.py
+++ sim_2.py
@@ -6,13 +6,19 @@ import torch.nn.functional as f
 
 torch.manual_seed(0)
 
-a = torch.rand(7,3)
-b = torch.zeros(7,3)
+a = torch.tensor([
+        [0, 3],
+        [0, 4],
+        [0, 6],
+        [1, 6],
+        [2, 5],
+        [3, 4],
+        [3, 6],
+        [4, 5]], device='cuda:0')
 
-final = torch.cat((a, b),1)
+b = torch.select(0, a[:,0])
+print(b)
 
-print(final)
-final[:,0] = torch.where(a[:,0] > 0, 5,-1)
-print(final)
+
 
 
