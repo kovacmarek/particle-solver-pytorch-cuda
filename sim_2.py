9147a190e80f1d87341216d0f48590af9267f125
Marek Kovac <marekkovac321@gmail.com>
2022-02-15 19:35:39 +0100

    particles pushing away from each other

--- sim_2.py
+++ sim_2.py
@@ -6,18 +6,12 @@ import torch.nn.functional as f
 
 torch.manual_seed(0)
 
-a = torch.tensor([
-        [0, 3],
-        [0, 4],
-        [0, 6],
-        [1, 6],
-        [2, 5],
-        [3, 4],
-        [3, 6],
-        [4, 5]], device='cuda:0')
-
-b = torch.select(0, a[:,0])
-print(b)
+x = torch.zeros(5, 3)
+# t = torch.tensor([3,4,6,6,5,4,6,5], dtype=torch.float)
+t = torch.rand(5,3)*10
+index = torch.tensor([0,0,0,1,2,3,3,4])
+x.index_add_(0, index, t)
+print(x)
 
 
 
