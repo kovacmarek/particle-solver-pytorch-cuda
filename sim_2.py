6443c091cc0b8bfadfb04b46adf777bba8e2860c
Marek Kovac <marekkovac321@gmail.com>
2022-03-02 19:31:46 +0100

    Let's do some Memory optimisation

--- sim_2.py
+++ sim_2.py
@@ -6,7 +6,12 @@ import torch.nn.functional as f
 
 torch.manual_seed(0)
 
-zero = 0.0
-print(type(zero))
+selected = torch.rand(7,3, device='cuda:1')*10
+
+if ((selected + 2) < selected) & ((selected - 2) > selected):
+    cdist = torch.cdist(selected, selected, p=2)
+print(cdist)
+
+
 
 
