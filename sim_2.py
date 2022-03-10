2d9d1bd276418f2d5e5b52f546ad97cd855fe333
Marek Kovac <marekkovac321@gmail.com>
2022-03-11 00:04:00 +0100

    reflection is correct, find intersect needs more precision

--- sim_2.py
+++ sim_2.py
@@ -6,11 +6,6 @@ import torch.nn.functional as f
 
 torch.manual_seed(0)
 
-selected = torch.rand(7,3, device='cuda:1')*10
-
-if ((selected + 2) < selected) & ((selected - 2) > selected):
-    cdist = torch.cdist(selected, selected, p=2)
-print(cdist)
 
 
 
